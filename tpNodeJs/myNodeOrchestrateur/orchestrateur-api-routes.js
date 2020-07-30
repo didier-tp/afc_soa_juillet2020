f01d6fd73423579bcaaca3bec0943df22b67e8c3
didier-tp <didier@ici_ou_la.fr>
2020-07-30 08:48:32 +0200

    orchestrateur nodeJs v1 (callback hell)

--- tpNodeJs/myNodeOrchestrateur/orchestrateur-api-routes.js
+++ tpNodeJs/myNodeOrchestrateur/orchestrateur-api-routes.js
@@ -11,20 +11,22 @@ apiRouter.route('/orchestrateur-api/public/propositionPret')
 	var url1 = "http://localhost:8282/taux-api/public/tauxInteretCourant?nbMois="+nbMois;
 	request(url1, function(error, response, body) {
 		//callback recuperant la reponse numero 1
-		console.log("status=" + response.statusCode);
-		console.log("body au format json=" + body);
+		console.log("status req1=" + response.statusCode);
+		console.log("body req1 au format json=" + body);
 		var tauxInteretResponseJs = JSON.parse(body);
 		var url2 = "http://localhost:8282/mensualite-api/public/mensualite?nbMois="
 		           +nbMois+"&taux=" + tauxInteretResponseJs.tauxInteret+"&montant=" + montant;
 	    request(url2, function(error, response, body) {
 			//sous callback recuperant la reponse numero 2
-			//...	
+			console.log("status req2=" + response.statusCode);
+		    console.log("body req2 au format json=" + body);
+			var calculMensualiteResponseJs = JSON.parse(body);
 			var jsRes = {
 				nbMois : nbMois ,
 				montant : montant , 
 				tauxInteret : tauxInteretResponseJs.tauxInteret,
-				mensualite : 127.34,
-				fraisDossier : 100
+				mensualite : calculMensualiteResponseJs.mensualite,
+				fraisDossier : 100.0
 			};
 			res.send(jsRes);		
 		});
